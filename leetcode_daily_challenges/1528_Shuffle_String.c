619785f7660af787dc49b42702203fe7e7b81739
vansh1999 <vansh.bhardwaj1999@gmail.com>
2020-10-17 17:34:41 +0530

    adding CP leetcode challenge solution

--- leetcode_daily_challenges/1528_Shuffle_String.c
+++ leetcode_daily_challenges/1528_Shuffle_String.c
@@ -1,20 +1,20 @@
-1528. Shuffle String
-    https : //leetcode.com/problems/shuffle-string/
+// 1528. Shuffle String
+// https : //leetcode.com/problems/shuffle-string/
 
-            // Problem Statement
+// Problem Statement
 
-            // Given a string s and an integer array indices of the same length.
+// Given a string s and an integer array indices of the same length.
 
-            // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
-            // Return the shuffled string.
+// Return the shuffled string.
 
-            // Example - goto link
+// Example - goto link
 
-            // Accepted Code
+// Accepted Code
 
-            char *
-            restoreString(char *s, int *indices, int indicesSize)
+char *
+restoreString(char *s, int *indices, int indicesSize)
 {
 
     char *s_new = (char *)malloc(sizeof(char) * (indicesSize + 1));
