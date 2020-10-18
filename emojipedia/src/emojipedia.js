98dadc08a645256dd6ca4e02208776c001d395f1
Naman Agarwal <thenamanagarwal@gmail.com>
2020-10-18 11:22:39 +0530

    Added Emojipedia Mini Project - An encyclopedia for emojis - made using React.

---
+++ emojipedia/src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous.	"
+  },
+  {
+    id: 5,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors."
+  },
+  {
+    id: 6,
+    emoji: "😊",
+    name: "Smiling Face With Smiling Eyes",
+    meaning:
+      "Is grinning shyly and embarrassed. The red cheeks are an expression of joy. Represents happiness, contentment, peace of mind and gratitude. Text-based variant: ^^."
+  },
+  {
+    id: 7,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 8,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning:
+      "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo."
+  },
+  {
+    id: 9,
+    emoji: "😓",
+    name: "Face With Cold Sweat",
+    meaning:
+      "Stress or worry about a bad experience. Things went different than they should have. The school is stressing or there is still so much to do in the office. You are tired, but still have so much work to do.	"
+  }
+];
+
+export default emojipedia;
