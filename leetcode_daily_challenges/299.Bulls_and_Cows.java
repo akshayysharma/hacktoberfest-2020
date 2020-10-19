7d2dd0a3acd739937276bd67a6af49deabaf8d5d
Sukriti Chaudhary <36128135+sukriti1112@users.noreply.github.com>
2020-10-19 17:53:01 +0530

    Update 299.Bulls_and_Cows.java

--- leetcode_daily_challenges/299.Bulls_and_Cows.java
+++ leetcode_daily_challenges/299.Bulls_and_Cows.java
@@ -1,3 +1,16 @@
+/*
+Problem Statement: 
+You are playing the Bulls and Cows game with your friend.
+You write down a secret number and ask your friend to guess what the number is. When your friend makes a guess, you provide a hint with the following info:
+
+The number of "bulls", which are digits in the guess that are in the correct position.
+The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position.
+Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+
+The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+*/
+
 class Solution {
     public String getHint(String secret, String guess) {
         int count[] = new int[10];
