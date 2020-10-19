1f7b8b1abb81dbd8ffac08319a0c1cb16d0f73e6
Andika Nugraha <m.andika.nugraha@gmail.com>
2020-10-19 14:47:45 +0700

    Update breaking-the-records.java

--- breaking_the_record/breaking-the-records.java
+++ breaking_the_record/breaking-the-records.java
@@ -1,3 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+* Description
+* It must return an integer array containing the numbers of times she broke her records. 
+* Index  is for breaking most points records, and index  is for breaking least points records.
+* BreakingRecords has the following parameter(s): 
+* - scores: an array of integers
+ *--------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------
+* Input Format
+* The first line contains an integer , the number of games.
+* The second line contains n space-separated integers describing the respective 
+  values of score-(0), score-(1), .... score-(n-1).
+*--------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------
+* Output Format
+* Print two space-seperated integers describing the respective numbers of times the best(highest) 
+  score increased and the worst (lowest) score decreased.
+*--------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------
+* Sample Input
+* First line    : 9
+* Second line   : 10 5 20 20 4 5 2 25 1
+*--------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------
+* Sample Output
+* 2 4
+*--------------------------------------------------------------------------------------------*/
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -13,7 +45,7 @@ public class Solution {
         for(int score_i=0; score_i < n; score_i++){
             score[score_i] = in.nextInt();
         }
-        // your code goes here
+
         int max = score[0];
         int maxCnt = 0;
         int min = score[0];
