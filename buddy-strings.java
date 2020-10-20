12a7c3cc18833c523bde900f73c599bedca8e1cb
Neha Labhasetwar <nehalabhasetwar17@gmail.com>
2020-10-20 21:36:45 +0530

    updated buddy_strings.java

--- buddy-strings.java
+++ buddy-strings.java
@@ -2,6 +2,19 @@
 solution to Buddy Strings at leetcode in Java.
 Author : Neha Labhasetwar.
 Ref : https://leetcode.com/problems/buddy-strings
+
+
+Description:
+Given two strings A and B of lowercase letters, return true if you can swap two letters in A so the result is equal to B, otherwise, return false.
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at A[i] and A[j]. For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+Example1:
+Input: A = "ab", B = "ba"
+Output: true
+
+Example2:
+Input: A = "ab", B = "ab"
+Output: false
 */
 
 class Solution {
